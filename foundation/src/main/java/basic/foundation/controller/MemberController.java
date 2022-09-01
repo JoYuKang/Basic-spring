f84596e2904e5adb656f3ca379b562c3f47c701d
YU KANG JO <46198324+JoYuKang@users.noreply.github.com>
2022-09-01 11:59:57 +0900

    # 회원가입(기본) DB 미구현

---
+++ foundation/src/main/java/basic/foundation/controller/MemberController.java
@@ -0,0 +1,41 @@
+package basic.foundation.controller;
+
+import basic.foundation.domain.Member;
+import basic.foundation.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm){
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        memberService.signup(member);
+        System.out.println("name " + memberForm.getName());
+        return "redirect:/";
+    }
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> memberList = memberService.findMembers();
+        model.addAttribute("members",memberList);
+        return "members/memberList";
+    }
+}
--- foundation/src/main/java/basic/foundation/repository/MemoryMemberRepository.java
+++ foundation/src/main/java/basic/foundation/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package basic.foundation.repository;
 
 import basic.foundation.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
